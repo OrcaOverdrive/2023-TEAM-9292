eb7928ceeb613221b847682f63a5f0cf7e362240
Andy-Sheng314 <shengandypi@gmail.com>
2023-03-03 11:54:58 -0800

    Update Robot.cpp

--- 2023 FRC 9292 Code/src/main/cpp/Robot.cpp
+++ 2023 FRC 9292 Code/src/main/cpp/Robot.cpp
@@ -15,31 +15,47 @@
 #include <frc/drive/DifferentialDrive.h>
 #include <frc/BuiltInAccelerometer.h>
 #include <frc/Timer.h>
+#include <frc/motorcontrol/Spark.h>
 #include "rev/CANSparkMax.h"
 #include "cameraserver/CameraServer.h"
 #include <networktables/NetworkTableEntry.h>
 #include <frc/shuffleboard/SimpleWidget.h>
 #include <frc/shuffleboard/ComplexWidget.h>
+#include "frc/Compressor.h"
+#include "frc/DoubleSolenoid.h"
+
+#include <frc/motorcontrol/Spark.h>
 
 using namespace std;
 
-// Settings for drive train. Speeed, ID, rotation speed and joystick control.
-int leftDriveTrainID = 20, rightDriveTrainID = 21;
-int armID = 23, clawID = 22;
+// ID for motors
+int leftLeadMotorID = 60, rightLeadMotorID = 61; // I don't know why they needed to be changed, but it works
+int leftFollowMotorID = 24, rightFollowMotorID = 25;
+int armLeadID = 22, armFollowID = 23;
+
+// PWM port for linear actuator
+int clawChannel = 0;
+
 // int placeholderC = 24, placeholderD = 25, placeholderE = 26;
 
 double driveSpeed = 0;
 double turnSpeed = 0;
 double armSpeed = 0;
 
-double fastSetPoint = 0.70;
+double fastSetPoint = 0.8;
 double perciseSetPoint = 0.55;
 double armSetPoint = 0.25;
 double rotateSpeed = 0.5;
 
-double increment = 0.04;
-double armIncrement = 0.02;
-double iPercent = 0.2;
+double increment = 0.05;
+
+double armIncrement = 0.015;
+
+double movePercent = 0.2;
+double turnPercent = 0.05;
+double armPercent = 0.2;
+
+double autoSpeed; // hard-coded speed
 double maxAutoSpeed = 0.5; // not in use
 
 bool isJoystick = false;
@@ -57,11 +73,22 @@ frc::Timer *timer = new frc::Timer();
 frc::BuiltInAccelerometer rioAccelerometer(frc::Accelerometer::Range::kRange_2G);
 
 frc::XboxController controller(0);
-frc::Joystick joystick(1);
-rev::CANSparkMax m_leftDriveTrain{leftDriveTrainID, rev::CANSparkMax::MotorType::kBrushed};
-rev::CANSparkMax m_rightDriveTrain{rightDriveTrainID, rev::CANSparkMax::MotorType::kBrushed};
-rev::CANSparkMax m_arm{armID, rev::CANSparkMax::MotorType::kBrushed};
-rev::CANSparkMax m_claw{clawID, rev::CANSparkMax::MotorType::kBrushed};
+// frc::Joystick joystick(1);
+
+rev::CANSparkMax m_leftLeadMotor{leftLeadMotorID, rev::CANSparkMax::MotorType::kBrushed};
+rev::CANSparkMax m_rightLeadMotor{rightLeadMotorID, rev::CANSparkMax::MotorType::kBrushed};
+
+rev::CANSparkMax m_leftFollowMotor{leftFollowMotorID, rev::CANSparkMax::MotorType::kBrushed};
+rev::CANSparkMax m_rightFollowMotor{rightFollowMotorID, rev::CANSparkMax::MotorType::kBrushed};
+
+rev::CANSparkMax m_armLeadMotor{armLeadID, rev::CANSparkMax::MotorType::kBrushed};
+rev::CANSparkMax m_armFollowMotor{armFollowID, rev::CANSparkMax::MotorType::kBrushed};
+
+frc::Spark m_claw{clawChannel};
+
+frc::DoubleSolenoid pcmSolenoid{10, frc::PneumaticsModuleType::REVPH, 13, 15};
+
+frc::Compressor pcmCompresser{10, frc::PneumaticsModuleType::REVPH};
 
 // placeholders:
 /*
@@ -70,7 +97,7 @@ rev::CANSparkMax motorD{placeholderD, rev::CANSparkMax::MotorType::kBrushless};
 rev::CANSparkMax motorE{placeholderE, rev::CANSparkMax::MotorType::kBrushless};
 */
 
-frc::DifferentialDrive m_driveTrain{m_leftDriveTrain, m_rightDriveTrain};
+frc::DifferentialDrive m_driveTrain{m_leftLeadMotor, m_rightLeadMotor};
 
 // Shuffleboard testing code
 frc::ShuffleboardTab& testingTab = frc::Shuffleboard::GetTab("Testing");
@@ -80,22 +107,25 @@ frc::SimpleWidget& testingWidget = testingTab.Add("Random Axis", controller.GetR
 
 void toggleBool(bool *boolVar, bool buttonPressed) {
   if (buttonPressed) {
-    *boolVar = !(*boolVar);
+    *boolVar = !(*boolVar);   
   }
 }
 
-double incrementAmount(double diff, double i) {
+double incrementAmount(double diff, double i, double percent) {
   if (diff > 0) {
-    return iPercent*diff + i;
+    return percent*diff + i;
   } else if (diff < 0) {
-    return iPercent*diff - i;
+    return percent*diff - i;
   }
+
+  // alternetive code??
+  // return iPercent*diff + (diff/abs(diff))*i;
 }
 
 signed int getBuiltInAccelerometer() {
   int val_x = (int) (rioAccelerometer.GetZ()*1000 - 15);
   // printf("val_x: %d\n", val_x);
-  printf("prev = %i, cur = %i\n", q.back(), val_x);
+  // printf("prev = %i, cur = %i\n", q.back(), val_x);
   if (val_x - q.back() >= 50) {
     q.push(q.back()+50);
     q.pop();
@@ -121,16 +151,30 @@ signed int getBuiltInAccelerometer() {
 
 void Robot::RobotInit() {
   m_chooser.SetDefaultOption("Default", kAutoNameDefault);
-  m_chooser.AddOption("Custom", kAutoNameCustom);
+  m_chooser.AddOption("Periodic Accelerometer", "Periodic Accelerometer");
+  m_chooser.AddOption("Push Cube", "Push Cube");
+  m_chooser.AddOption("Charge Station", "Charge Station");
+
   frc::CameraServer::StartAutomaticCapture();
   cs::CvSink cvSink = frc::CameraServer::GetVideo();
   cs::CvSource outputStream = frc::CameraServer::PutVideo("Blur",640,480);
   
   frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
-  m_leftDriveTrain.RestoreFactoryDefaults();
-  m_rightDriveTrain.RestoreFactoryDefaults();
+  m_leftLeadMotor.RestoreFactoryDefaults();
+  m_leftFollowMotor.RestoreFactoryDefaults();
+  m_rightLeadMotor.RestoreFactoryDefaults();
+  m_rightFollowMotor.RestoreFactoryDefaults();
 
-  m_rightDriveTrain.SetInverted(true);
+  m_rightLeadMotor.SetInverted(true);
+  m_rightFollowMotor.SetInverted(true);
+
+  m_leftFollowMotor.Follow(m_leftLeadMotor);
+  m_rightFollowMotor.Follow(m_rightLeadMotor);
+  m_armFollowMotor.Follow(m_armLeadMotor);
+
+  // pnematics
+  pcmCompresser.EnableDigital();
+  pcmSolenoid.Set(frc::DoubleSolenoid::Value::kForward);
 }
 
 /**
@@ -163,14 +207,14 @@ void Robot::AutonomousInit() {
   } else {
     // Defult Auto
     timer->Start();
-    printf("starting\n");
+    // printf("starting\n");
   }
 }
 
 void Robot::AutonomousPeriodic() {
-  if (m_autoSelected == kAutoNameCustom) {
+  if (m_autoSelected == "Periodic Accelerometer") {
     signed int angle = getBuiltInAccelerometer();
-    printf("x, onRamp: %i, %s\n", angle, autoOnRamp ? "true" : "false");
+    // printf("x, onRamp: %i, %s\n", angle, autoOnRamp ? "true" : "false");
     if (angle < -300) {
       autoOnRamp = true;
     }
@@ -179,20 +223,57 @@ void Robot::AutonomousPeriodic() {
     } else if (autoOnRamp && angle > -50) {
       m_driveTrain.TankDrive(0, 0);
     }
+  } else if (m_autoSelected == "Push Cube") {
+    float time = timer->Get().value();
 
-  } else {
+    if (time <= 0.8) {
+      autoSpeed = 0;
+      m_armLeadMotor.Set(-0.15);
+
+    } else if (time <= 6) {
+      m_armLeadMotor.Set(0);
+      autoSpeed = 0.5;
+    } else {
+      autoSpeed = 0;
+    }
+
+    m_driveTrain.TankDrive(autoSpeed, autoSpeed);
+
+  } else if (m_autoSelected == "Charge Station") {
     // Default Auto
-    units::time::second_t curTime = timer->Get();
-    float time = curTime.value();
-    printf("hello\n");
-    printf("current time: %f\n", time);
+    // units::time::second_t curTime = timer->Get();
+    float time = timer->Get().value();
+    // printf("current time: %f\n", time);
 
+    if (time <= 0.8) {
+      autoSpeed = 0;
+      m_armLeadMotor.Set(-0.15);
     if (time <= 3) {
-      m_driveTrain.TankDrive(0.3, 0.3);
+      autoSpeed = 0.8;
+      m_armLeadMotor.Set(0);
+    } else if (time <= 5.25) {
+      autoSpeed = 0.45;
+    // } else if (time <= 6) {
+    //   autoSpeed = 0;
+    // } else if(time <= 9.5) {
+    //   autoSpeed = -0.7;
+    } else {
+      autoSpeed = 0;
+    }
+
+    // printf("hello\n");
+
+    m_driveTrain.TankDrive(autoSpeed, autoSpeed);
+  } else {
+    float time = timer->Get().value();
+
+    if (time <= 6) {
+      m_driveTrain.TankDrive(0.5, 0.5);
     } else {
       m_driveTrain.TankDrive(0, 0);
     }
   }
+  } 
 }
 
 void Robot::TeleopInit() {}
@@ -217,7 +298,7 @@ void Robot::TeleopPeriodic() {
   //   driveSpeed -= increment;
   // }
 
-  driveSpeed += incrementAmount(targetForwardSpeed - driveSpeed, increment);
+  driveSpeed += incrementAmount(targetForwardSpeed - driveSpeed, increment, movePercent);
 
   // adjust turn speed
   // if (targetTurnSpeed > turnSpeed) {
@@ -228,7 +309,7 @@ void Robot::TeleopPeriodic() {
   //   turnSpeed -= increment;
   // }
 
-  turnSpeed += incrementAmount(targetTurnSpeed - turnSpeed, increment);
+  turnSpeed += incrementAmount(targetTurnSpeed - turnSpeed, increment, turnPercent);
 
   // adjust armSpeed
   // if (targetArmPosition > armSpeed) {
@@ -239,7 +320,7 @@ void Robot::TeleopPeriodic() {
   //   armSpeed -= armIncrement;
   // }
 
-  armSpeed += incrementAmount(targetArmPosition - armSpeed, increment);
+  armSpeed += incrementAmount(targetArmPosition - armSpeed, increment, armPercent);
 
   // printf("x = %i\n", getBuiltInAccelerometer());
   
@@ -249,14 +330,25 @@ void Robot::TeleopPeriodic() {
     // frc::SmartDashboard::PutString("Test","Test"); 
   // }
 
-  // m_arm controls up and down of arm
-  m_claw.Set(controller.GetRawAxis(2));
+  // pnematics control
+  if (controller.GetRawButtonPressed(4)) {
+    pcmSolenoid.Toggle();
+  }
 
-  // m_claw controls claw
-  if (controlsReverse) {
-    m_arm.Set(-armSpeed);
+  if (controller.GetRawButton(7)) {
+    m_claw.Set(1);
+  } else if (controller.GetRawButton(8)) {
+    m_claw.Set(-1);
   } else {
-    m_arm.Set(armSpeed);
+    m_claw.Set(0);
+  }
+    
+  // m_claw.Set(controller.GetRawAxis(2));
+  
+  if (controlsReverse) {
+    m_armLeadMotor.Set(-armSpeed);
+  } else {
+    m_armLeadMotor.Set(armSpeed);
   }
 
   // if (isJoystick) {
@@ -264,13 +356,18 @@ void Robot::TeleopPeriodic() {
   //   m_driveTrain.ArcadeDrive(joystick.GetRawAxis(1)*driveSpeed, joystick.GetRawAxis(0)*((joystick.GetRawAxis(1) < 0) ? -rotateSpeed : rotateSpeed));
   // } else {
   //   // If not joystick, enable controller control for drive train
-  // }
+  //    // }
 
   if (controlsReverse) {
-    m_driveTrain.ArcadeDrive(-driveSpeed, turnSpeed);
+    m_driveTrain.ArcadeDrive(-driveSpeed, turnSpeed, false);
   } else {
-    m_driveTrain.ArcadeDrive(driveSpeed, turnSpeed);
+    m_driveTrain.ArcadeDrive(driveSpeed, turnSpeed, false);
   }
+
+  // Emergency
+  // if (controller.GetRawButton(3)) {
+  //   m_driveTrain.ArcadeDrive(controller.GetLeftY()*0.7, controller.GetLeftX()*0.7);
+  // }
 
   // put important data onto smartdashboard
   frc::SmartDashboard::PutNumber("targetForwardSpeed", targetForwardSpeed);
@@ -280,6 +377,10 @@ void Robot::TeleopPeriodic() {
   frc::SmartDashboard::PutNumber("Accelerometer", getBuiltInAccelerometer());
   frc::SmartDashboard::PutBoolean("controlsReverse", controlsReverse);
   frc::SmartDashboard::PutBoolean("persisionMode", persisionMode);
+  frc::SmartDashboard::PutBoolean("Pnumatics Toggle", controller.GetRawButton(4));
+
+  // put differential drive data (hopefully appears as a widget), not tested
+  frc::SmartDashboard::PutData("Differential Drive", &m_driveTrain);
 }
 
 void Robot::DisabledInit() {}
